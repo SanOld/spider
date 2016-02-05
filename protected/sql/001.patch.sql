059fccb017e79f2b3b837c1cf55c1cb1d3948fe7
Kayfat <kayfat@Kayfats-MacBook-Pro.local>
2016-02-05 15:13:41 +0200

    patch changes

--- protected/sql/001.patch.sql
+++ protected/sql/001.patch.sql
@@ -7,3 +7,13 @@ INSERT INTO `spi_user_type` (`type`, `name`) VALUES ('d', 'District');
 INSERT INTO `spi_user_type` (`type`, `name`) VALUES ('a', 'Senat');
 
 ALTER TABLE `spi_user` ADD COLUMN `type_id` INT(11) NOT NULL ALTER `type`;
+
+UPDATE`spi_user` SET `type_id`='1' WHERE `id`='1';
+
+CREATE TABLE `spi_user_type_right` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `type_id` INT NOT NULL,
+  `page_id` INT NOT NULL,
+  `can_view` TINYINT(1) NOT NULL DEFAULT 0,
+  `can_edit` TINYINT(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`));
