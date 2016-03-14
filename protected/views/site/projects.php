e9181ff6f488cf4e7943fff8818de9574981dc01
Kayfat <kayfat@Kayfats-MacBook-Pro.local>
2016-03-14 15:51:34 +0200

    projects fix

--- protected/views/site/projects.php
+++ protected/views/site/projects.php
@@ -173,12 +173,12 @@ $this->breadcrumbs = array('Projekte');
                         <div spi-hint text="_hint.school_type_id" class="has-hint"></div>
                         <div class="wrap-hint">
                           <ui-select ng-disabled="!$select.items.length" ng-model="project.school_type_id"
-                                     name="school_type_id" required on-select="updateSchools()">
+                                     name="school_type_id" required on-select="updateSchools();updateCode();">
                             <ui-select-match placeholder="{{$select.disabled ? '(No items available)' :'(Please choose)'}}">
-                              {{$select.selected.name}}
+                              {{$select.selected.fullName}}
                             </ui-select-match>
                             <ui-select-choices repeat="item.id as item in schoolTypes | filter: $select.search">
-                              <span ng-bind-html="item.name | highlight: $select.search"></span>
+                              <span ng-bind-html="item.fullName | highlight: $select.search"></span>
                             </ui-select-choices>
                           </ui-select>
                         </div>
@@ -287,11 +287,11 @@ $this->breadcrumbs = array('Projekte');
               </div>
               <div class="form-group group-btn m-t-15">
                   <div class="col-lg-2">
-                      <a class="btn btn-icon btn-danger btn-lg sweet-4"><i class="fa fa-trash-o"></i></a>
+                      <a class="btn btn-icon btn-danger btn-lg sweet-4" ng-click="remove()"><i class="fa fa-trash-o"></i></a>
                   </div>
                   <div class="col-lg-10 text-right pull-right">
-                      <button class="btn w-lg cancel-btn" data-dismiss="modal">Abbrechen</button>
-                      <button class="btn w-lg custom-btn" data-dismiss="modal">Speichern</button>
+                      <button class="btn w-lg cancel-btn" data-dismiss="modal" ng-click="cancel()">Abbrechen</button>
+                      <button class="btn w-lg custom-btn" data-dismiss="modal" ng-click="submitFormProjects()">Speichern</button>
                   </div>
               </div>
           </ng-form>
