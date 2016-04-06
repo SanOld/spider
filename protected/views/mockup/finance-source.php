27179c694506f5e4a6cc7edc565d4a25d277b9fc
Kayfat <kayfat@Kayfats-MacBook-Pro.local>
2016-04-06 11:28:02 +0300

    IRSP-473

--- protected/views/mockup/finance-source.php
+++ protected/views/mockup/finance-source.php
@@ -34,7 +34,7 @@
 								<h1 class="panel-title col-lg-6">Fördertöpfe</h1>
 								<div class="pull-right heading-box-print">
 									<a href="javascript:window.print()">Drucken<i class="ion-printer"></i></a>
-									<button class="btn w-lg custom-btn" data-target="#modal-2" data-toggle="modal">Fördertöpf hinzufügen</button>
+									<button class="btn w-lg custom-btn" data-target="#modal-2" data-toggle="modal">Fördertopf hinzufügen</button>
 								</div>
 							</div>
 							<div class="panel-body edit-user">
