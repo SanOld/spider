6e9bc11918bebacf3a47e71658bcc9ef10da72c4
Oleg Vystavnoy <ovistavnoy@dyn.com>
2016-05-17 23:23:49 +0300

    add rights for admin

---
+++ protected/sql/019.patch.sql
@@ -0,0 +1,3 @@
+INSERT INTO spi_user_type_right (type_id, page_id, can_show, can_view, can_edit) VALUES (1, 4,  1, 1, 1);
+INSERT INTO spi_user_type_right (type_id, page_id, can_show, can_view, can_edit) VALUES (1, 15, 1, 1, 1);
+INSERT INTO spi_user_type_right (type_id, page_id, can_show, can_view, can_edit) VALUES (1, 16, 1, 1, 1);
