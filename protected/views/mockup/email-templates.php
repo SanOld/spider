7386c5837af74963c2ffc8ff8f6aec76059e2294
Kayfat <kayfat@Kayfats-MacBook-Pro.local>
2016-05-31 11:30:05 +0300

    IRSP-658

--- protected/views/mockup/email-templates.php
+++ protected/views/mockup/email-templates.php
@@ -18,7 +18,7 @@
 				
 			<div class="container">
 				<ul class="breadcrumb p-0">
-					<li><a href="/">Home</a></li>
+					<li><a href="/dashboard">Home</a></li>
 					<li class="active">Email-Vorlagen</li>
 				</ul>
 			</div>
