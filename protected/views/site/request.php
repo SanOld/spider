49b9b383ed30a71fce2f23c48e7bcca1cead94c1
Kayfat <kayfat@Kayfats-MacBook-Pro.local>
2016-05-31 10:44:50 +0300

    IRSP-402

--- protected/views/site/request.php
+++ protected/views/site/request.php
@@ -394,7 +394,7 @@ $this->breadcrumbs = array('Anträge');
 											</div>
 										</div>
 										<div class="col-lg-8">
-											<h4>Auflage vom Senat</h4>
+											<h4>Auflage</h4>
 											<textarea class="form-control custom-height-textarea" placeholder="Tragen Sie den Text hier ein" ng-model="request.senat_additional_info" class="form-control"></textarea>
 										</div>
 									</div>
