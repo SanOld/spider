154362b0631021122b63b33b9fd26696131cc1ed
Oleg Vystavnoy <ovistavnoy@dyn.com>
2016-06-15 12:48:54 +0300

    list status, fixed save fin plan

--- protected/models/RequestStatus.php
+++ protected/models/RequestStatus.php
@@ -13,15 +13,22 @@ class RequestStatus extends BaseModel {
   }
 
   protected function doAfterSelect($result) {
-    $activity_ids = array();
+    $activity_ids   = array();
+    $deactivity_ids = array();
     foreach($result['result'] as &$row) {
       $row['virtual'] = 0;
       if(in_array($row['code'], array('open', 'in_progress', 'acceptable', 'accept'))) {
         $activity_ids[] = $row['id'];
       }
+      if($row['code'] == 'decline') {
+        $deactivity_ids[] = $row['id'];
+      }
+    }
+    if($deactivity_ids) {
+      array_unshift($result['result'], array('id' => implode(',', $deactivity_ids), 'code' => 'deactive_all', 'name' => '(Nicht aktiv)', 'virtual' => 1));
     }
     if($activity_ids) {
-      array_unshift($result['result'], array('id' => implode(',', $activity_ids), 'code' => 'active_all', 'name' => '(Active all)', 'virtual' => 1));
+      array_unshift($result['result'], array('id' => implode(',', $activity_ids), 'code' => 'active_all', 'name' => '(Aktiv)', 'virtual' => 1));
     }
     return $result;
   }
