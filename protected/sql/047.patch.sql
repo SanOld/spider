41ee7e829e06f58d4bcab2b02e7b856a83b2c990
emakeeva <emakeeva@itera-research.com>
2016-06-22 16:08:33 +0300

    IRSP-847 Popup should display actual information at once
    renamed file

--- protected/sql/047.patch.sql
+++ protected/sql/047.patch.sql
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 ALTER TABLE `spi_document_template_placeholder` ADD COLUMN `document_id` INT(11) NULL DEFAULT NULL;
 
 ALTER TABLE `spi_document_template_placeholder`
@@ -22,3 +23,6 @@ INSERT INTO `spi_document_template_placeholder` (`name`, `text`, `is_email`, `do
 =======
 INSERT INTO `spi_page_position` (`page_id`, `code`, `name`) VALUES ('16', 'header', 'Überschrift');
 >>>>>>> 43c6e3a... IRSP-558, IRSP-767, IRSP-850 Hilfe text
+=======
+INSERT INTO `spi_page_position` (`page_id`, `code`,`name`) VALUES ('20', 'header', 'Überschrift');
+>>>>>>> 76e7995... renamed file
