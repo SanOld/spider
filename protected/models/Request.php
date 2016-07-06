eb720f44e6c68ec325bec9d7be463c33f5c1706d
ashehovtsov <ashehovtsov@itera-research.com>
2016-07-06 22:27:38 +0300

    fix mail

--- protected/models/Request.php
+++ protected/models/Request.php
@@ -373,23 +373,23 @@ class Request extends BaseModel {
     }
     
     if(safe($post, 'status_finance') == 'rejected') {
-//      $request = Yii::app() -> db -> createCommand()
-//        -> select('(SELECT code FROM spi_project WHERE id = rq.project_id) code, (SELECT email FROM spi_user WHERE id = rq.finance_user_id) finance_user_email')
-//        -> from('spi_request rq')
-//        -> where('rq.id=:id', array(':id' => $request_id))
-//        ->queryRow();
-//
-//      $emailParams = array(
-//          'request_code' => $request['code'],
-//          'part' => 'finanzplan',
-//          'comment' => safe($post, 'finance_comment'),
-//          'date' => date('H:i d.m.Y'),
-//          'url' => 'http://spider.dev/request/'.safe($post, 'request_id').'#finance-plan',
-//      );
-//
-//      if($request['finance_user_email']) {
-//        Email::sendMessageByTemplate('antrag_reject', $emailParams, $request['finance_user_email']);
-//      }
+      $request = Yii::app() -> db -> createCommand()
+        -> select('(SELECT code FROM spi_project WHERE id = rq.project_id) code, (SELECT email FROM spi_user WHERE id = rq.finance_user_id) finance_user_email')
+        -> from('spi_request rq')
+        -> where('rq.id=:id', array(':id' => $request_id))
+        ->queryRow();
+
+      $emailParams = array(
+          'request_code' => $request['code'],
+          'part' => 'finanzplan',
+          'comment' => safe($post, 'finance_comment'),
+          'date' => date('H:i d.m.Y'),
+          'url' => 'http://spider.dev/request/'.safe($post, 'request_id').'#finance-plan',
+      );
+
+      if($request['finance_user_email']) {
+        Email::sendMessageByTemplate('antrag_reject', $emailParams, $request['finance_user_email']);
+      }
     }
     
     if(safe($post, 'status_id') == 4 || safe($post, 'status_id') == 5 ) {
@@ -406,9 +406,9 @@ class Request extends BaseModel {
       );
 
       $template = safe($post, 'status_id') == 4?'antrag_acknowledge':'antrag_acknowledge';
-//      if($request['finance_user_email']) {
-//        Email::sendMessageByTemplate('antrag_acknowledge', $emailParams, $request['finance_user_email']);
-//      }
+      if($request['finance_user_email']) {
+        Email::sendMessageByTemplate('antrag_acknowledge', $emailParams, $request['finance_user_email']);
+      }
     }
     
 
