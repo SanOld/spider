bfa831046ed242ff18a3cce2163217c9d5b4265b
ashehovtsov <ashehovtsov@itera-research.com>
2016-07-07 12:43:23 +0300

    fix IRSP-483

--- protected/sql/055.patch.sql
+++ protected/sql/055.patch.sql
@@ -4,8 +4,8 @@ INSERT INTO spi_page (name, code, page_code, is_system) VALUE ('Benutzerliste', 
 
 SELECT @page_id:=id FROM `spi_page` WHERE `code`= 'user_lock';
 
-INSERT INTO spi_user_type_right (`type_id`, `page_id`, `can_show`, `can_view`, `can_edit`) VALUE (1, @page_id, 1, 1, 0);
-INSERT INTO spi_user_type_right (`type_id`, `page_id`, `can_show`, `can_view`, `can_edit`) VALUE (2, @page_id, 1, 1, 0);
+INSERT INTO spi_user_type_right (`type_id`, `page_id`, `can_show`, `can_view`, `can_edit`) VALUE (1, @page_id, 1, 1, 1);
+INSERT INTO spi_user_type_right (`type_id`, `page_id`, `can_show`, `can_view`, `can_edit`) VALUE (2, @page_id, 1, 1, 1);
 INSERT INTO spi_user_type_right (`type_id`, `page_id`, `can_show`, `can_view`, `can_edit`) VALUE (3, @page_id, 1, 1, 0);
 INSERT INTO spi_user_type_right (`type_id`, `page_id`, `can_show`, `can_view`, `can_edit`) VALUE (4, @page_id, 0, 0, 0);
 INSERT INTO spi_user_type_right (`type_id`, `page_id`, `can_show`, `can_view`, `can_edit`) VALUE (5, @page_id, 0, 0, 0);
