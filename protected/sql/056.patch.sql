4eb9a9260e69c59b252269773afc43316ddc4ab7
ashehovtsov <ashehovtsov@itera-research.com>
2016-07-08 18:12:31 +0300

    fix patch 056

--- protected/sql/056.patch.sql
+++ protected/sql/056.patch.sql
@@ -1,1 +1,3 @@
-UPDATE `spi_document_template_placeholder` SET `text` = 'Benutzername' WHERE `id` = '7';
+
+UPDATE `spi_document_template_placeholder` SET `text` = 'Name des antragsstellenden Trägers' WHERE `name` = 'TRAEGER';
+INSERT INTO `spi_document_template_placeholder` (`name`, `text`, `is_email`) VALUES ('TRAGERADRESSE', 'Adresse des antragsstellenden Trägers', '0');
