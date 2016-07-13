f63c7d0b9d2e4619f99ef3d2481f94298af203be
ashehovtsov <ashehovtsov@itera-research.com>
2016-07-13 15:04:32 +0300

    fix IRSP-911

--- protected/sql/059.patch.sql
+++ protected/sql/059.patch.sql
@@ -1,4 +1,3 @@
-ALTER TABLE `spi_request`
-	ADD COLUMN `status_id_ta` INT(11) NOT NULL DEFAULT '1' AFTER `status_id`;
-	ADD COLUMN `status_concept_ta` VARCHAR(45) NOT NULL DEFAULT 'unfinished' AFTER `status_concept`;
-	ADD COLUMN `status_goal_ta` VARCHAR(45) NOT NULL DEFAULT 'unfinished' AFTER `status_goal`;
+ALTER TABLE `spi_request`	ADD COLUMN `status_id_ta` INT(11) NOT NULL DEFAULT '1' AFTER `status_id`;
+ALTER TABLE `spi_request`	ADD COLUMN `status_concept_ta` VARCHAR(45) NOT NULL DEFAULT 'unfinished' AFTER `status_concept`;
+ALTER TABLE `spi_request`	ADD COLUMN `status_goal_ta` VARCHAR(45) NOT NULL DEFAULT 'unfinished' AFTER `status_goal`;
