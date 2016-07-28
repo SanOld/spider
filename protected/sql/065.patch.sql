b65e46b2579ec5ceb04a642dba29f733c77d7f23
ashehovtsov <ashehovtsov@itera-research.com>
2016-07-28 12:54:04 +0300

    fix IRSP-967

---
+++ protected/sql/065.patch.sql
@@ -0,0 +1,8 @@
+SELECT @page_id:=id FROM `spi_page` WHERE `code`= 'contact';
+
+INSERT INTO spi_user_type_right (`type_id`, `page_id`, `can_show`, `can_view`, `can_edit`) VALUE (1, @page_id, 1, 1, 1);
+INSERT INTO spi_user_type_right (`type_id`, `page_id`, `can_show`, `can_view`, `can_edit`) VALUE (2, @page_id, 1, 1, 1);
+INSERT INTO spi_user_type_right (`type_id`, `page_id`, `can_show`, `can_view`, `can_edit`) VALUE (3, @page_id, 1, 1, 1);
+INSERT INTO spi_user_type_right (`type_id`, `page_id`, `can_show`, `can_view`, `can_edit`) VALUE (4, @page_id, 1, 0, 0);
+INSERT INTO spi_user_type_right (`type_id`, `page_id`, `can_show`, `can_view`, `can_edit`) VALUE (5, @page_id, 1, 0, 0);
+INSERT INTO spi_user_type_right (`type_id`, `page_id`, `can_show`, `can_view`, `can_edit`) VALUE (6, @page_id, 1, 0, 0); 
