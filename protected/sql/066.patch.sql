6d2e49d85daf5894fd36e008dff94226f62be58f
emakeeva <emakeeva@itera-research.com>
2016-07-28 16:33:22 +0300

    patch added
    renamed

--- protected/sql/066.patch.sql
+++ protected/sql/066.patch.sql
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 SELECT @page_id:=id FROM `spi_page` WHERE `code`= 'contact';
 
 INSERT INTO spi_user_type_right (`type_id`, `page_id`, `can_show`, `can_view`, `can_edit`) VALUE (1, @page_id, 1, 1, 1);
@@ -6,3 +7,7 @@ INSERT INTO spi_user_type_right (`type_id`, `page_id`, `can_show`, `can_view`, `
 INSERT INTO spi_user_type_right (`type_id`, `page_id`, `can_show`, `can_view`, `can_edit`) VALUE (4, @page_id, 1, 0, 0);
 INSERT INTO spi_user_type_right (`type_id`, `page_id`, `can_show`, `can_view`, `can_edit`) VALUE (5, @page_id, 1, 0, 0);
 INSERT INTO spi_user_type_right (`type_id`, `page_id`, `can_show`, `can_view`, `can_edit`) VALUE (6, @page_id, 1, 0, 0); 
+=======
+INSERT INTO spi_project_school (project_id, school_id)
+SELECT prj.id, 0 FROM spi_project prj LEFT JOIN spi_project_school prs ON prj.id = prs.project_id WHERE prs.school_id IS NULL;
+>>>>>>> 829061a... patch added
