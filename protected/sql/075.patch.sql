709cafc0fd34d5f15b8c8355599d0f9482d17e6c
ashehovtsov <ashehovtsov@itera-research.com>
2016-08-09 17:05:09 +0300

    fix IRSP-995

--- protected/sql/075.patch.sql
+++ protected/sql/075.patch.sql
@@ -1,4 +1,4 @@
 UPDATE `spi_page_position` SET `name` = 'Projektdaten / Überschrift' WHERE `code` = 'projectData'; 
-UPDATE `spi_page_position` SET `name` = 'finData / Überschrift' WHERE `code` = 'Finanzplan / Überschrift'; 
-UPDATE `spi_page_position` SET `name` = 'conceptData / Überschrift' WHERE `code` = 'Konzept / Überschrift'; 
-UPDATE `spi_page_position` SET `name` = 'goalData / Überschrift' WHERE `code` = 'Entwicklungsziele / Überschrift'; 
+UPDATE `spi_page_position` SET `name` = 'Finanzplan / Überschrift' WHERE `code` = 'finData';
+UPDATE `spi_page_position` SET `name` = 'Konzept / Überschrift' WHERE `code` = 'conceptData';
+UPDATE `spi_page_position` SET `name` = 'Entwicklungsziele / Überschrift' WHERE `code` = 'goalData'; 
