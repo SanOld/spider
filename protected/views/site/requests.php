f88948331c7f0f0366426882c6a841f82f89ae33
ashehovtsov <ashehovtsov@itera-research.com>
2016-08-11 16:20:52 +0300

    fix IRSP-1019

--- protected/views/site/requests.php
+++ protected/views/site/requests.php
@@ -379,23 +379,24 @@ $this->breadcrumbs = array('Anträge');
               </div>
             </div>
             <div class="col-lg-3 p-0">
-              <label>Fälligkeitstermin</label>
-            </div>
-            <div class="col-lg-3 p-0">
-              <div class="input-group">
-                <input type="text" ng-click="dp_due_date_is_open = !dp_due_date_is_open" ng-model="form.due_date" uib-datepicker-popup="dd.MM.yyyy" datepicker-append-to-body="true" show-button-bar="false" is-open="dp_due_date_is_open" datepicker-options="dateOptions" required class="form-control datepicker" placeholder="dd.mm.yyyy">
-                <span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i></span>
-              </div>
-            </div>
-          </div>
-          <br>
-          <div class="holder-datepicker text-right">
-            <div class="col-lg-3 p-0">
               <label>Abgabedatum</label>
             </div>
             <div class="col-lg-3 p-0">
               <div class="input-group">
                 <input type="text" ng-click="dp_end_fill_is_open = !dp_end_fill_is_open" ng-model="form.end_fill" uib-datepicker-popup="dd.MM.yyyy" datepicker-append-to-body="true" show-button-bar="false" is-open="dp_end_fill_is_open" datepicker-options="dateOptions" required class="form-control datepicker" placeholder="dd.mm.yyyy">
+                <span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i></span>
+              </div>
+            </div>
+
+          </div>
+          <br>
+          <div class="holder-datepicker text-right">
+            <div class="col-lg-3 p-0">
+              <label>Fälligkeitstermin</label>
+            </div>
+            <div class="col-lg-3 p-0">
+              <div class="input-group">
+                <input type="text" ng-click="dp_due_date_is_open = !dp_due_date_is_open" ng-model="form.due_date" uib-datepicker-popup="dd.MM.yyyy" datepicker-append-to-body="true" show-button-bar="false" is-open="dp_due_date_is_open" datepicker-options="dateOptions" required class="form-control datepicker" placeholder="dd.mm.yyyy">
                 <span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i></span>
               </div>
             </div>
