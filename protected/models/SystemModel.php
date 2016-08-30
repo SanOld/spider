2057814a24229f7a7ed3f0a1caa3ce64f5fe5d01
ashehovtsov <ashehovtsov@itera-research.com>
2016-08-30 19:22:05 +0300

    fix SystemModel

--- protected/models/SystemModel.php
+++ protected/models/SystemModel.php
@@ -84,7 +84,7 @@ class SystemModel extends BaseModel
             DROP TRIGGER IF EXISTS `{$tableName}_A{$operation['code']}`;
 
             CREATE
-                /*[DEFINER = { 'spider_user' | CURRENT_USER }]*/
+                /*[DEFINER = { user | CURRENT_USER }]*/
                 TRIGGER `{$tableName}_A{$operation['code']}` {$operation['when']} ON `{$tableName}` 
                 FOR EACH ROW BEGIN    
                 DECLARE ev_id INT; 	     
@@ -107,9 +107,11 @@ class SystemModel extends BaseModel
         }
         Yii::app ()->db->createCommand ()->update ( 'spi_audit_setting', array('hash' => $hash), 'id=:id', array (':id' => $table['id'] ));
       }
-      header ( 'Content-Type: application/json' );
+//      header ( 'Content-Type: application/json' );
 //      echo json_encode ( array('results' => 'done') );
-       echo json_encode ($trigger) ;
+       echo ($trigger) ;
+       echo (1111) ;
+
       exit ();
     }
 
