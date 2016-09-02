6c67657e08f567469c912642673523418bb215cf
emakeeva <emakeeva@itera-research.com>
2016-09-02 19:18:16 +0300

    IRSP-475 - placeholders added

---
+++ protected/sql/090.patch.sql
@@ -0,0 +1,40 @@
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('4', '3', '0'); 
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('6', '3', '0'); 
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('7', '3', '0'); 
+
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('4', '4', '0'); 
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('6', '4', '0'); 
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('7', '4', '0'); 
+
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('4', '5', '0'); 
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('6', '5', '0'); 
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('7', '5', '0'); 
+
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('4', '26', '0'); 
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('6', '26', '0'); 
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('7', '26', '0');
+
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('4', '27', '0'); 
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('6', '27', '0'); 
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('7', '27', '0');
+
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('4', '28', '0'); 
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('6', '28', '0'); 
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('7', '28', '0');
+
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('4', '29', '0'); 
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('6', '29', '0'); 
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('7', '29', '0');
+
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('4', '30', '0'); 
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('6', '30', '0'); 
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('7', '30', '0');
+
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('4', '31', '0'); 
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('6', '31', '0'); 
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('7', '31', '0');
+
+
+UPDATE `spi_document_template` SET `is_prototype` = '0' WHERE `id` = '5'; 
+UPDATE `spi_document_template` SET `is_prototype` = '0' WHERE `id` = '13'; 
+UPDATE `spi_document_template` SET `is_prototype` = '0' WHERE `id` = '14'; 
