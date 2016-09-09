c73481a611adde7fd955a078d9075c8943d321a3
emakeeva <emakeeva@itera-research.com>
2016-09-09 11:26:55 +0300

    IRSP-1085 - placeholders added

---
+++ protected/sql/094.patch.sql
@@ -0,0 +1,4 @@
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('5', '4', '0'); 
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('5', '3', '0'); 
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('5', '5', '0'); 
+INSERT INTO `spi_document_type_placeholder` (`document_type_id`, `placeholder_id`, `email_document_id`) VALUES ('5', '90', '0');
